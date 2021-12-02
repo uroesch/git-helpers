d34147be0191ce5e931caa9974e13949bf53c827
Urs Roesch <github@bun.ch>
2021-12-02 10:12:14 +0100

    Put default branches into array
    Summary:
      * Put default branches into an array to make
        them easily extendable with non-standard
        named branches.

--- tests/includes.bash
+++ tests/includes.bash
@@ -4,9 +4,9 @@
 # Globals
 # -----------------------------------------------------------------------------
 export PATH=${BATS_TEST_DIRNAME}/../bin:${PATH}
-export GIT_AUTOREBASE_VERSION="v0.5.0"
-export GIT_FPC_VERSION="v0.5.2"
-export GIT_OPUSH_VERSION="v0.5.0"
+export GIT_AUTOREBASE_VERSION="v0.6.0"
+export GIT_FPC_VERSION="v0.6.0"
+export GIT_OPUSH_VERSION="v0.6.0"
 export GIT_WALKTREE_VERSION="v0.1.2"
 export STRIP_TRAILING_WHITESPACE_VERSION="v0.4.0"
 export SHA256SUM_WHITESPACE_FILE="506dcd8e1fdd674bf75c87191145642a440cd4aa11a95fc37c4197f7a4e5118d"
